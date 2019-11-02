54bc847e60a7d2f7ac71550e564a034319ea2547
Michael Johne <mjohne@users.noreply.github.com>
2019-11-02 12:16:07 +0100

    Add files via upload

--- PlanetoidDB/Properties/AssemblyInfo.cs
+++ PlanetoidDB/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
@@ -37,3 +38,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.21")]
 [assembly: AssemblyFileVersion("0.4.0.21")]
+[assembly: NeutralResourcesLanguage("en")]
