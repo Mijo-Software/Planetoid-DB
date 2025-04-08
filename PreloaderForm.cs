6ebca32aa4c117f2d42696cc1fe9b4d920400428
Michael de Radobýl <mjohne@users.noreply.github.com>
2025-04-08 18:09:31 +0200

    Add files via upload

---
+++ PreloaderForm.cs
@@ -0,0 +1,184 @@
+﻿using System.Diagnostics;
+using Krypton.Toolkit;
+using NLog;
+
+namespace Planetoid_DB
+{
+	/// <summary>
+	/// A form that gets the file MPCORB.DAT.
+	/// </summary>
+	[DebuggerDisplay(value: "{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+	public partial class PreloaderForm : KryptonForm
+	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		#region constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AppInfoForm"/> class.
+		/// </summary>
+
+		public PreloaderForm()
+		{
+			InitializeComponent();
+			this.KeyDown += new KeyEventHandler(PreloaderForm_KeyDown);
+			this.KeyPreview = true; // Ensures the form receives key events before the controls
+		}
+
+		/// <summary>
+		/// Copies the specified text to the clipboard and displays a confirmation message.
+		/// </summary>
+		/// <param name="text">The text to be copied.</param>
+		private static void CopyToClipboard(string text)
+		{
+			try
+			{
+				Clipboard.SetText(text: text);
+				_ = MessageBox.Show(text: I10nStrings.CopiedToClipboard, caption: I10nStrings.InformationCaption, buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "File not found");
+				ShowErrorMessage(message: $"File not found: {ex.Message}");
+				Environment.ExitCode = 1;
+			}
+		}
+
+		/// <summary>
+		/// Sets the status bar text.
+		/// </summary>
+		/// <param name="text">The main text to be displayed on the status bar.</param>
+		/// <param name="additionalInfo">Additional information to be displayed alongside the main text.</param>
+		private void SetStatusbar(string text, string additionalInfo = "")
+		{
+			if (!string.IsNullOrWhiteSpace(value: text))
+			{
+				labelInformation.Enabled = true;
+				labelInformation.Text = string.IsNullOrWhiteSpace(value: additionalInfo) ? text : $"{text} - {additionalInfo}";
+			}
+		}
+
+		/// <summary>
+		/// Clears the status bar text.
+		/// </summary>
+		private void ClearStatusbar()
+		{
+			labelInformation.Enabled = false;
+			labelInformation.Text = string.Empty;
+		}
+
+		#endregion
+
+		#region local methods
+
+		/// <summary>
+		/// Returns a string representation of the object for the debugger.
+		/// </summary>
+		/// <returns>A string representation of the object.</returns>
+		private string GetDebuggerDisplay() => ToString();
+
+		/// <summary>
+		/// Displays an error message.
+		/// </summary>
+		/// <param name="message">The error message.</param>
+		private static void ShowErrorMessage(string message)
+		{
+			_ = MessageBox.Show(text: message, caption: I10nStrings.ErrorCaption, buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+		}
+
+		#endregion
+
+		#region form event handler
+
+		/// <summary>
+		/// Fired when the form loads.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
+		private void PreloaderForm_Load(object sender, EventArgs e) => ClearStatusbar_Leave(sender: sender, e: e);
+
+		/// <summary>
+		/// Fired when the form closes.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="FormClosedEventArgs"/> instance that contains the event data.</param>
+		private void PreloaderForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Dispose();
+			DialogResult = DialogResult.Cancel;
+		}
+
+		#endregion
+
+		#region enter event handlers
+
+		/// <summary>
+		/// Called when the mouse pointer moves over a control.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
+		private void SetStatusbar_Enter(object sender, EventArgs e)
+		{
+			if (sender is Control control && control.AccessibleDescription != null)
+			{
+				SetStatusbar(text: control.AccessibleDescription);
+			}
+		}
+
+		#endregion
+
+		#region leave event handlers
+
+		/// <summary>
+		/// Called when the mouse pointer leaves a control.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
+		private void ClearStatusbar_Leave(object sender, EventArgs e) => ClearStatusbar();
+
+		#endregion
+
+		#region click event handlers
+
+		//
+
+		#endregion
+
+		#region double-click event handler
+
+		/// <summary>
+		/// Called when a control is double-clicked to copy the text to the clipboard.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
+		private void CopyToClipboard_DoubleClick(object sender, EventArgs e)
+		{
+			ArgumentNullException.ThrowIfNull(argument: sender);
+			if (sender is Control control)
+			{
+				CopyToClipboard(text: control.Text);
+			}
+		}
+
+		#endregion
+
+		#region KeyDown event handler
+
+		/// <summary>
+		/// Handles the KeyDown event of the ExportDataSheetForm.
+		/// Closes the form when the Escape key is pressed.
+		/// </summary>
+		/// <param name="sender">The event source.</param>
+		/// <param name="e">The <see cref="EventArgs"/> instance that contains the event data.</param>
+		private void PreloaderForm_KeyDown(object? sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				DialogResult = DialogResult.Cancel;
+				Close();
+			}
+		}
+
+		#endregion
+	}
+}
